96f20ab3028757003648982aebe0b5e04e50704b
LuisRicado77 <luissayas2018@gmail.com>
2024-09-04 22:24:38 -0500

    structure of the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,36 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(miApp());
+
+class miApp extends StatelessWidget {
+  const miApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Mi App",
+      home: Inicio(),
+    );
+  }
+}
+
+class Inicio extends StatefulWidget {
+  const Inicio({super.key});
+
+  @override
+  State<Inicio> createState() => _InicioState();
+}
+
+class _InicioState extends State<Inicio> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("App Sena flutter"),
+      ),
+      body: Center(
+        child: Image.asset('assets/logo.jpeg'), // Aquí defines la ruta
+      ),
+    );
+  }
+}
